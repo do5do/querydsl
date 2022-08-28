5de3900dc66abf5a3aefaa7f11620da302f3e5b9
dodo <do151015@gmail.com>
2022-08-28 16:48:09 +0900

    init

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,40 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class QuerydslApplicationTests {
+	@PersistenceContext
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello; // 위와 같은 의미. QHello에 가면 자기 자신을 구현해 놓음.
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
